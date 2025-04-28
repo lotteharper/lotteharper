1036ff6f77c8107694dbab722557cbd3da33779b
Jasper Holton <jasper.camber.holton@gmail.com>
2025-04-28 01:26:05 -0700

    open source

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 USER_NAME='team'
 PROJECT_NAME='lotteh'
-DIR_PATH="/home/$USER_NAME/$PROJECT_NAME/"
-ls ${DIR_PATH}
+GIT_URL_PROJ='ch.git'
+REPO_DATE='meme'
+PROJECT_GIT_URL="${GIT_URL_PROJ%.*}.$REPO_DATE"
+echo "$PROJECT_GIT_URL"
