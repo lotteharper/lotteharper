d0521db21aa74dd8c3ee623f88243589560cb3ee
Jasper Holton <jasper.camber.holton@gmail.com>
2025-04-28 02:23:48 -0700

    open source

--- test.sh
+++ test.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-USER_NAME='team'
-PROJECT_NAME='lotteh'
-GIT_URL_PROJ='ch.git'
-REPO_DATE='meme'
-PROJECT_GIT_URL="${GIT_URL_PROJ%.*}.$REPO_DATE"
-echo "$PROJECT_GIT_URL"
+string="example.string+with.dots"
+result="${string%%+*}"
+echo "$result"  # Output: example
