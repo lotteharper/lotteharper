a034ed4e0d0bbcf435cdc35db1646401bb0f8a78
Jasper Holton <jasper.camber.holton@gmail.com>
2025-08-01 12:05:52 -0700

    open source

--- events/models.py
+++ events/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 import uuid
+from meetings.models import Meeting
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
@@ -11,6 +12,7 @@ class Event(models.Model):
     location = models.CharField(max_length=255, blank=True)
     participants = models.CharField(max_length=1000)
     link = models.CharField(max_length=255)
+    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE, related_name='events', null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -20,8 +22,8 @@ class Event(models.Model):
         from django.conf import settings
         from django.urls import reverse
         from meetings.models import Meeting
-        meeting = Meeting.objects.create(identifier=uuid.uuid4(), created_by=creator)
-        self.link = settings.BASE_URL + reverse('meetings:meeting', kwargs={'meeting_id': str(meeting.identifier)})
+        self.meeting = Meeting.objects.create(identifier=uuid.uuid4(), created_by=creator)
+        self.link = settings.BASE_URL + reverse('meetings:meeting', kwargs={'meeting_id': str(self.meeting.identifier)})
         self.description = self.description + '\n*** Click the link below to start your meeting\n' + self.link
         self.save()
 
@@ -32,5 +34,7 @@ class Event(models.Model):
         link = settings.BASE_URL + reverse('events:add-to-calendar', kwargs={'event_id': self.identifier})
         participants = self.participants.replace(' ','')
         import pytz
+        from django.contrib.auth.models import User
         for email in self.participants.split(','):
-            send_email(email, 'You have been scheduled to join a meeting with {}'.format(settings.SITE_NAME), 'Please click the link below download the attached invite.\n\n<a href="{}">{}</a>\n\nThe invite contains a link to a video meeting. Please click the link in the description of the invite in your calendar and join the meeting from your phone, tablet, or other device.\nThe meeting begins at {}.\nTo join, please click the link below or the link in the invite.\n\n<a href="{}">{}</a>'.format(link, link, self.start_time.astimezone(pytz.timezone(settings.TIME_ZONE)).strftime('%m/%d/%Y at %H:%M:%S'), self.link, self.link))
+            u = User.objects.filter(email=email).order_by('-profile__last_seen').first()
+            send_email(email, 'You have been scheduled by {} to join a video meeting with {}'.format(self., 'Hello {},\nYou have been scheduled to join a video meeting with {} {}'.format(settings.SITE_NAME), 'Please click the link below download the attached invite and add it to your calendar for reference by opening it on your device.\n\n<a href="{}">{}</a>\n\nThe invite contains a link to a video meeting. Please click the link in the description of the invite in your calendar and join the meeting from your phone, tablet, or other device.\nWhen the meeting starts, please click the link below or the link in the invite.\n\n<a href="{}">{}</a>'.format('' if not u else u.profile.name, link, link, self.start_time.astimezone(pytz.timezone(settings.TIME_ZONE)).strftime('%m/%d/%Y at %H:%M:%S'), self.link, self.link))
