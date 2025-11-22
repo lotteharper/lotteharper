a964975d03740a2c72b3caf54b055b9a8c102c4a
Jasper Holton <jasper.camber.holton@gmail.com>
2025-11-22 00:24:41 -0800

    open source

--- contact/forms.py
+++ contact/forms.py
@@ -14,8 +14,6 @@ class ContactForm(forms.ModelForm):
         from feed.middleware import get_current_request
         from translate.translate import translate
         request = get_current_request()
-        print(request)
-        print(str(request))
         self.fields['name'].label = translate(request, 'Your name', src='en')
         self.fields['email'].label = translate(request, 'Your email address', src='en')
         self.fields['phone'].label = translate(request, 'Your phone number', src='en')
